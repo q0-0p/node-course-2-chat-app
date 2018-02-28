edb2fddca72ea50fe3d652d583d37aa1cfe78129
yousif al-bayaty <yalbayaty@zomaron.com>
2018-02-28 11:49:09 -0500

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -23,22 +23,43 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) =>{
-console.log('New user connected');
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
-
-socket.on('createMessage', (message) =>{
-    console.log('createMessage', message);
-    io.emit('newMessage',{
-        from:message.from,
-        text:message.text,
-        createAt:new Date().getTime()
+    //socket.emit from Admin text Welcome to the chat app
+    //socket.broadcast.emit from Admin text New user joined
+    socket.emit('newMessage', {
+     from: "Admin",
+     text: "Welcome to the chat app"  ,
+     createdAt: new Date().getTime()
+      
     });
-});
-socket.on('disconnect', () =>{
-    console.log('Disconnected from server');
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New user joined',
+        createdAt: new Date().getTime()
+        
+    })
+
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
+        // socket.broadcast.emit('newMessage', {
+        //     from:message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
+    });
+    socket.on('disconnect', () => {
+        console.log('Disconnected from server');
+    })
 })
-})
-server.listen(port, () =>{
+server.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
 });
